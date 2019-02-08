58bdd3c92efc1d00e04217f7104b98ec01d9b1e9
Michael Bottini <mbottini1@gmail.com>
2019-02-08 04:40:31 +0000

    Add attempt at parsing a FixedTimeWorkout
    Probably not correct; I forgot the length of the headers.

--- DataTypes/FixedTimeWorkout.hs
+++ DataTypes/FixedTimeWorkout.hs
@@ -3,10 +3,24 @@ module DataTypes.FixedTimeWorkout where
 import qualified DataTypes.TableEntry as Te
 import qualified DataTypes.FixedHeader as Fh
 import qualified DataTypes.TimeFrame as Tf
+import qualified Utils
+
+import Data.Word
 
 data FixedTimeWorkout = FixedTimeWorkout {
     tableEntry :: Te.TableEntry,
     header :: Fh.FixedHeader,
     frames :: [Tf.TimeFrame]
 } deriving(Show)
+ 
+parseFixedTimeWorkout :: [Word8] -> [Word8] -> FixedTimeWorkout
+parseFixedTimeWorkout teLst rawFrameLst = FixedTimeWorkout {
+    tableEntry = newEntry,
+    header = newHeader,
+    frames = newFrames
+}
+    where newEntry = Te.parseTableEntry teLst
+          rawFrameChunk = Utils.grabChunk (Te.recordOffset newEntry) (Te.recordSize newEntry) rawFrameLst
+          newHeader = Fh.parseFixedHeader (Utils.grabChunk 0 52 rawFrameChunk)
+          newFrames = map Tf.parseTimeFrame (Utils.splitAll 52 . drop 52 $ rawFrameChunk)
 
