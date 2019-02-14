3f7c843e9b81f6127eebcd49d2e55a93c067708e
Michael Bottini <mbottini1@gmail.com>
2019-02-14 17:34:31 +0000

    Add parsing for FixedDistanceWorkouts
    Also added a Const file for the sizes of various structures in the
    binary blob.

---
+++ DataTypes/Consts.hs
@@ -0,0 +1,13 @@
+module DataTypes.Consts where
+
+fixedHeaderSize :: Int
+fixedHeaderSize = 50
+
+intervalHeaderSize :: Int
+intervalHeaderSize = 52
+
+frameSize :: Int
+frameSize = 32
+
+variableIntervalFrameSize :: Int
+variableIntervalFrameSize = 48
--- DataTypes/FixedDistanceWorkout.hs
+++ DataTypes/FixedDistanceWorkout.hs
@@ -3,6 +3,9 @@ module DataTypes.FixedDistanceWorkout where
 import qualified DataTypes.TableEntry as Te
 import qualified DataTypes.FixedHeader as Fh
 import qualified DataTypes.DistanceFrame as Df
+import qualified Utils
+import qualified DataTypes.Consts as Consts
+import Data.Word
 
 data FixedDistanceWorkout = FixedDistanceWorkout {
     tableEntry :: Te.TableEntry,
@@ -10,3 +13,16 @@ data FixedDistanceWorkout = FixedDistanceWorkout {
     frames :: [Df.DistanceFrame]
 } deriving(Show)
 
+parseFixedDistanceWorkout :: [Word8] -> [Word8] -> FixedDistanceWorkout
+parseFixedDistanceWorkout hs ds = FixedDistanceWorkout {
+    tableEntry = te,
+    header = Fh.parseFixedHeader chunk,
+    frames = map Df.parseDistanceFrame . 
+             Utils.splitAll Consts.frameSize .
+             drop Consts.fixedHeaderSize .
+             Utils.grabChunk offset index $ ds
+}
+    where te = Te.parseTableEntry hs
+          chunk = Utils.grabChunk offset Consts.fixedHeaderSize ds
+          offset = Te.recordOffset te
+          index = Te.recordSize te
