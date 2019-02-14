637e79a13aba2bbc60acf5b495126e622dd286c5
Michael Bottini <mbottini1@gmail.com>
2019-02-14 22:52:48 +0000

    Add DistanceIntervalWorkout

---
+++ DataTypes/DistanceIntervalWorkout.hs
@@ -0,0 +1,28 @@
+module DataTypes.DistanceIntervalWorkout where
+
+import qualified DataTypes.TableEntry as Te
+import qualified DataTypes.FixedIntervalHeader as Fih
+import qualified DataTypes.DistanceIntervalFrame as Dif
+import qualified Utils
+import qualified DataTypes.Consts as Consts
+import Data.Word
+
+data DistanceIntervalWorkout = DistanceIntervalWorkout {
+    tableEntry :: Te.TableEntry,
+    header :: Fih.FixedIntervalHeader,
+    frames :: [Dif.DistanceIntervalFrame]
+} deriving(Show)
+
+parseDistanceIntervalWorkout :: [Word8] -> [Word8] -> DistanceIntervalWorkout
+parseDistanceIntervalWorkout hs ds = DistanceIntervalWorkout {
+    tableEntry = te,
+    header = Fih.parseFixedIntervalHeader chunk,
+    frames = map Dif.parseDistanceIntervalFrame . 
+             Utils.splitAll Consts.frameSize .
+             drop Consts.fixedHeaderSize .
+             Utils.grabChunk offset index $ ds
+}
+    where te = Te.parseTableEntry hs
+          chunk = Utils.grabChunk offset Consts.intervalHeaderSize ds
+          offset = Te.recordOffset te
+          index = Te.recordSize te
