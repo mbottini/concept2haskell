9d5d78de3b651b23beaafbdfc99a5138893631db
Michael Bottini <mbottini1@gmail.com>
2019-03-11 19:00:51 -0700

    Fix DistanceIntervalFrame

--- DataTypes/DistanceIntervalFrame.hs
+++ DataTypes/DistanceIntervalFrame.hs
@@ -24,5 +24,6 @@ parseDistanceIntervalFrame ws = DistanceIntervalFrame {
 
 instance ToJSON DistanceIntervalFrame where
     toJSON dif = object [
+        "type" .= "distance",
         "time" .= (Utils.tenthsToScientific . duration $ dif),
         "stroke_rate" .= (Utils.intToScientific . strokesPerMinute $ dif)]
